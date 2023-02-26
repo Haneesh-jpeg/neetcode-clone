13d09fbd60d50923a3db7a26c3747aecd8ea1c9d
Haneesh <59616874+Haneesh-jpeg@users.noreply.github.com>
2023-02-26 11:33:11 +0530

    Added react app

--- server.js
+++ server.js
@@ -4,19 +4,11 @@ const app = express();
 
 app.use(express.static("frontend"));
 
-// app.get('/html',function(req,res){
-//     res.sendFile(__dirname + "/frontend/index.html");   
-// })
 
 app.get("/",function(req,res){
-    res.send("Hello");
+    res.sendFile(__dirname + "/frontend/index.html"); 
     
 })
-
-// app.get("/myname",function(req,res){
-//     res.send("<h1>Haneesh</h1>");
-// })
-
 
 
 app.listen(3000,function(){
