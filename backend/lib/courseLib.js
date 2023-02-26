5e6de77f5f39e5766bd26d8f902ef5e7add2f2e1
Haneesh <59616874+Haneesh-jpeg@users.noreply.github.com>
2023-02-26 15:11:29 +0530

    added update function

--- backend/lib/courseLib.js
+++ backend/lib/courseLib.js
@@ -5,6 +5,17 @@ module.exports.getAllCourses = async function(){
     
 }
 
+module.exports.createCourse = async function(courseInput){
+    const course = new courseModel(courseInput);
+    await course.save();   
+}
+
+
+module.exports.updateCourse = async function(courseId,updateInput){
+    
+    await courseModel.findOneAndUpdate({_id: courseId,updatedInput : updateInput})
+}
+
 module.exports.createFirstCourse = async function(){
     const courses = await courseModel.find({});
     if(courses && courses.length == 0){
