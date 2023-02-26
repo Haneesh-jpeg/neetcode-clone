305d26bf19c4c88d9d2a5efe27cad30d48302486
Haneesh <59616874+Haneesh-jpeg@users.noreply.github.com>
2023-02-26 11:01:19 +0530

    changed server and added nodemon

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static("frontend"));
+
+// app.get('/html',function(req,res){
+//     res.sendFile(__dirname + "/frontend/index.html");   
+// })
+
 app.get("/",function(req,res){
     res.send("Hello");
     
 })
 
-app.get("/myname",function(req,res){
-    res.send("<h1>Haneesh</h1>");
-})
+// app.get("/myname",function(req,res){
+//     res.send("<h1>Haneesh</h1>");
+// })
+
+
 
 app.listen(3000,function(){
     console.log("Server running on https://localhost:3000");
